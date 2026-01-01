f7dc98969f7250d6d381ee6e6257150fe29d3cc5
mennajko <yassermenna28@gmail.com>
2026-01-01 21:54:31 +0200

    Solve LeetCode 66 - Plus One

---
+++ 66-Plus-One/Solution.cs
@@ -0,0 +1,20 @@
+public class Solution {
+    public int[] PlusOne(int[] digits) {
+       
+        for (  int i = digits.Length - 1; i>=0 ; i-- ){
+            if (digits [i]< 9 ){
+                digits [i] ++;
+                return digits;
+            }
+            digits [i] = 0; 
+        }
+        
+        int[] result = new int [digits.Length + 1];
+        result [0]=1;
+        for (int i = 0 ; i < digits.Length ; i ++){
+            result [i +1 ] = digits [i];
+        }
+        return result ;
+
+    }
+}
